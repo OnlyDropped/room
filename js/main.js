94b9342b9f4915d507de771088fed087a504dfeb
OnlyDropped <asheburenkov@yandex.ru>
2021-07-31 01:18:53 +0300

    added slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
   $('.header__select').styler();
+  
+  $('.center').slick({
+    centerMode: true,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    dots: true,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt=""></button>',
+    //infinite: true,
+    //speed: 300,
+    centerPadding: '100px',
+    //slidesToShow: 3,
+  });
 
 });
